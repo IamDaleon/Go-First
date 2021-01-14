f679d93d38008881860770b1f0fbc8e1df93d686
Daleon L <daleon86@gmail.com>
2021-01-14 15:37:04 -0600

    First commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random playerChooser = new Random();
+            Console.WriteLine("Welcome to who goes first!");
+            Console.WriteLine("How many players are there?");
+            string amountOfPlayers = Console.ReadLine();
+            int figure = int.Parse(amountOfPlayers);
+            int selector = playerChooser.Next(1, figure);
+            Console.WriteLine($"Player {selector} goes first!");
+            Console.WriteLine("\nPress any key to exit.?");
+            Console.ReadKey(true);
+
+        }
+    }
+}
